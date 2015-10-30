0b411a566329116fe3c20eb1b044a2d79ff86569
Kev1nChen <chenyufei@live.com>
2015-10-30 10:01:02 -0500

    add shared data key as a todo reminder

--- PathToMath-iOS/PathToMath/Models/PMSharedData.swift
+++ PathToMath-iOS/PathToMath/Models/PMSharedData.swift
@@ -14,6 +14,7 @@ let kPMSharedDataProblemSetVersionKeyKey = "problemSetVersion"
 let kPMSharedDataLevelUpCorrectnessKeyKey = "levelUpCorrectness"
 let kPMSharedDataSupportContactNameKeyKey = "supportContactName"
 let kPMSharedDataSupportContactEmailKeyKey = "supportContactEmail"
+let kPMSharedDataAcknowledgementPageURLKeyKey = "acknowledgementPageURL"
 
 let kPMLocalDatastoreSharedDataPinName = "SharedData"
 
